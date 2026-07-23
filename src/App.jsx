8d0f54fa6b114b53a1f81cc568dd680b2bfdeeec
pratham.hack <pratham.1226667@gmail.com>
2026-07-23 19:35:14 +0530

    install gsap, @gsap/react and react-responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
