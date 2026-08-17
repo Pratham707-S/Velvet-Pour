d1265332a3828cc6fbc09d465f7a5560c69abfc8
pratham.hack <pratham.1226667@gmail.com>
2026-08-17 19:10:40 +0530

    add footer social icons and Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,22 @@ import Cocktails from './components/Cocktails.jsx'
 import About from './components/About.jsx'
 import Art from './components/Art.jsx'
 import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Cocktails />
-      <About />
-      <Art />
-      <Menu />
-    </main>
-  )
+ return (
+	<main>
+	 <Navbar />
+	 <Hero />
+	 <Cocktails />
+	 <About />
+	 <Art />
+	 <Menu />
+	 <Contact />
+	</main>
+ )
 }
 
 export default App
